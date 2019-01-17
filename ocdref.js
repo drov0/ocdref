665fc4336e0c6cecfb29fd7988a1ba0b816be72f
Martin Lees <drov0@users.noreply.github.com>
2019-01-17 15:30:40 +0100

    Added the power up and transfer methods

--- ocdref.js
+++ ocdref.js
@@ -89,17 +89,83 @@ function split_earnings()
 
             let share = Math.floor((parseFloat(ocdbfund[0].balance) * share_percentage) * 1000) / 1000;
 
-            if (share < 0.001)
+            // 0.002 because we will be sending a 0.001 transfer with a memo as well
+            if (share < 0.002)
                 share = 0;
 
             active_parents[i].share = share;
         }
 
 
-        console.log("yes")
+        for (let i = 0; i < active_parents.length; i++)
+        {
+            let u = 0;
+        }
 
 
 
+    });
+}
+
+/**
+ * Powers up x amounts of steem
+ * @param from Account from which the liquid steem will be drained
+ * @param to Account on which the liquid steem will be powered up (can be the same as the account from which the steem will be drained)
+ * @param amount Amount of tokens that you want to power up, note that it must be written like this : x.xxx unit eg : '1.265 STEEM' or '0.010 STEEM'
+ * @param active_key Active key of the from account.
+ * @returns {Promise<any>} Result of the operation.
+ */
+function power_up(from, to,  amount, active_key) {
+    return new Promise(async resolve => {
+
+        const privateKey = dsteem.PrivateKey.fromString(
+            active_key
+        );
+        const op = [
+            'transfer_to_vesting',
+            {
+                from: from,
+                to: to,
+                amount: amount,
+            },
+        ];
+        const result = await client.broadcast.sendOperations([op], privateKey).catch(function(error) {
+                console.error(error);
+            }
+        );
+
+        return resolve(result);
+
+    });
+}
+
+function transfer(from, to, amount, memo, active_key)
+{
+    return new Promise(resolve => {
+        const privateKey = PrivateKey.fromString(
+            active_key
+        );
+
+        //create transfer object
+        const transf = {
+            from: from,
+            to: to,
+            amount: amount,
+            memo: memo,
+        };
+
+        //broadcast the transfer
+
+        client.broadcast.transfer(transf, privateKey).then(
+            function (result) {
+                console.log(`Transferred ${amount} to ${to}`);
+                return resolve(result);
+            },
+            function (error) {
+                console.error(error);
+                return resolve(error);
+            }
+        );
     });
 }
 
